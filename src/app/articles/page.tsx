142118524e61898e99440c80c92fa0f968b26f7c
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2023-10-07 16:44:09 +0900

    [Add] 一覧表示実装

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
+
+export default async function Articles() {
+  // contentディレクトリ内のマークダウンファイル一覧を取得
+  const postsDirectory = path.join(process.cwd(), 'content');
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // 各ファイルの中身を取得
+  const posts = await Promise.all(
+    // 各ファイル情報を取得
+    fileNames.map(async (fileName) => {
+      const filePath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContents);
+
+      // slugとfrontmatter(title, date, description)を取得
+      return {
+        slug: fileName.replace('.md', ''),
+        frontmatter: data,
+      };
+    })
+  ).then((posts) =>
+    // 最新日付順に並び替え
+    posts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  );
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <p>{post.frontmatter.date}</p>
+            <Link href={`/article/${post.slug}`}>{post.frontmatter.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
