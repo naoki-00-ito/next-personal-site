dc4eb6e6c3c2b9fe1ee86c5968ead40151510bea
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2023-11-04 21:00:09 +0900

    [Add] top hero作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,12 @@
-import Image from 'next/image';
-import { getArticles } from '@/libs/getArticles';
-import AlticleList from '@/components/server/AlticleList';
-
-async function ArticlesCategory() {
-  const articles = await getArticles({ tag: 'sentry', max: 4 });
-
-  return (
-    <div className='p-index-articles'>
-      <AlticleList articles={articles} />
-    </div>
-  );
-}
-
-async function ArticlesTag() {
-  const articles = await getArticles({ tag: 'next', max: 4 });
-
-  return (
-    <div className='p-index-articles'>
-      <AlticleList articles={articles} />
-    </div>
-  );
-}
+import IndexHero from '@/components/client/index-hero';
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <IndexHero />
       <p>index</p>
 
-      <ArticlesCategory />
-
-      <ArticlesTag />
-    </div>
+      <div style={{ height: '100vh' }}></div>
+    </>
   );
 }
