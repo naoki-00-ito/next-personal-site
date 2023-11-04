04707b011ae69233539b7e0a31857ae4b3c35a3b
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2023-11-04 21:16:55 +0900

    [Add] top hero story作成

---
+++ src/stories/client/indexHero.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import indexHero from '@/components/client/indexHero';
+
+const meta = {
+  title: 'Components/Client/indexHero',
+  component: indexHero,
+  // tags: ['autodocs'],
+} satisfies Meta<typeof indexHero>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Sample: Story = {};
+
+Sample.decorators = [
+  (Story) => (
+    <div style={{ paddingBottom: '50vh' }}>
+      <Story />
+    </div>
+  ),
+];
